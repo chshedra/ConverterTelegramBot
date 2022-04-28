53662175abba0908ad7565fa1810880442ef84e6
chshedra <shedrin2001@gmail.com>
2022-04-28 22:29:54 +0700

    Реализован проект бизнес-логики по обработке и созданию файлов

---
+++ FileConverter/PdfHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Aspose;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+
+namespace FileHandler
+{
+	public class PdfHandler
+	{
+		public byte[] ConvertToPdf(string text)
+		{
+			var document = new Document();
+
+			var page = document.Pages.Add();
+
+			page.Paragraphs.Add(new TextFragment(text));
+
+			var memoryStream = new MemoryStream();
+
+			document.Save(memoryStream, SaveFormat.Pdf);
+
+			return memoryStream.ToArray();
+		}
+	}
+}
